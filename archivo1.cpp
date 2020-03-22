53ddb7a32e20d5f4a2cfa70e7daca50cbb752110
CesarJardines <eduardroid.98@gmail.com>
2020-03-21 21:23:57 -0600

    agregue stuff

---
+++ archivo1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int numero;
+    cout<<"Ingresa un numero mi perro: \n";
+    cin>>numero;
+    if(numero>=10){
+        cout<<"Bro, el numero tiene mas de dos numeros";
+    }else{
+        cout<<"tiene solo un numero";
+    }
+
+
+    return 0;
+}
