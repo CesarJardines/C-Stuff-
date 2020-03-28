6189eb12d371b80204bfe7ea70e4b80c1d591932
CesarJardines <eduardroid.98@gmail.com>
2020-03-27 19:59:42 -0600

    agrego reto

---
+++ Omega Pu++/Pastel.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	int a,aux,total,i,aux2;
+	
+	cin>> a;
+
+	while(i < a){
+		cin>>aux;
+		cout<<"Area a comprar: " << aux*a <<endl;
+		i++;
+	}
+
+
+	return 0;
+}
+
