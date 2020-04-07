b691a948254d296b1eda97db27025af9743f3f23
CesarJardines <eduardroid.98@gmail.com>
2020-04-07 15:13:16 -0500

    Agrego tabla

---
+++ TablaCinco.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	int suma = 5;
+
+	for (int i = 1; i <= 10; ++i){
+		n = suma * i;
+		cout<<n<<endl;
+	}
+
+	cout<<"La suma de los 5 numeros es: " <<suma<<endl;
+
+	return 0;
+}
