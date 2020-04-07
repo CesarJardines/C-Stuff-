6304e51fc30b3de78031b85f041eae073ba6e6ca
CesarJardines <eduardroid.98@gmail.com>
2020-04-07 15:04:37 -0500

    añadi triangulo

---
+++ Triangulo.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int base, altura,superficie, n,contador;
+	contador = 0;
+	cin>>n;
+	for (int i = 0; i < n; ++i){
+		cin>>base>>altura;
+		superficie = (base * altura) / 2;
+
+		cout<<"Base: "<<base<<" Altura: "<<altura<<" superficie: "<<superficie<<endl;
+		if(superficie > 12){
+			contador++;
+		}
+	}
+
+	cout<<"Triangulos con superficie mayor a 12: "<<contador<<endl;
+
+
+
+	return 0; 
+}
