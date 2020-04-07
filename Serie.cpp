f807366585328ca135a7ba200ca2deebd55c5915
CesarJardines <eduardroid.98@gmail.com>
2020-04-06 22:55:39 -0500

    ASD

---
+++ Serie.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n = 25;
+	int once =11;
+	int i = 0;
+	int total;
+	while(i < n){
+		cout<<once<<endl;
+		once = once + 11;
+		i++;
+	}	
+}
