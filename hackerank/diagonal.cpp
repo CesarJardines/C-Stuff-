c7eeaf8c3113901f7403e59e2afcc5ac67754ef3
CesarJardines <eduardroid.98@gmail.com>
2020-06-20 16:51:29 -0500

    Solucion simple

--- hackerank/diagonal.cpp
+++ hackerank/diagonal.cpp
@@ -1,9 +1,24 @@
 #include <bits/stdc++.h>
-
+#include <iostream>
+using namespace std;
 int main(){
+	int n, suma1 = 0, suma2 = 0, suma = 0;
+	cin>>n;
+	int arr[n][n];
 
-	
+	for(int i = 0; i < n ; i++){
+		for(int j = 0; j < n; j++){
+			cin>>arr[i][j];
+			if(i == j){
+				suma1 += arr[i][j];
+			}if(i + j == (n-1)){
+				suma2 += arr[i][j];
+			}
+		}
+	}
 
+	suma = abs(suma1 - suma2);
+	cout<<suma<<endl;
 
 	return 0;
 }
