b37841f48681593950b04ea0c0d84f5a740f5d05
CesarJardines <eduardroid.98@gmail.com>
2020-06-20 22:18:42 -0500

    Agrego ejercicio 6

---
+++ hackerank/Stairs.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n ;
+	string stair = "#", s, espacio = " ",espacio2;
+	cin>>n;
+//	string arr[n];
+
+	for (int i = 0; i <= n; ++i){
+		/* code */
+		for (int j = 0; j <= n; ++j){
+			/* code */
+			if (j <= n-1){
+				cout<<'#';
+			}else{
+				cout<<' ';
+			}
+			cout<<' ';
+		}
+	}
+
+	
+	return 0;
+}
